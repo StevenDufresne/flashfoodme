d392108cfa06bfdf7c9c67c089236e1e611dbc9f
Irfan Pirbhai <irfan.pirbhai@gmail.com>
2013-05-25 13:01:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashfoodme::Application.config.secret_token = '49feb0e1e6532ed5613248308572d07f4bf165b325b0f95228e8f7bf2211c1ebfd00ccf6baab6fb974e5ca7927817d42cd1bd5ff571e33b075ce0295f2e48661'
